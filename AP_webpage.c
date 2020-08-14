e0c06c7639e798bc9efc71f6cb79e23828b6272e
adityathakekar <adi.thakekar@gmail.com>
2020-08-14 12:46:19 +0530

    dynamic login page for iot devices
    webpage-dynamic login

---
+++ AP_webpage.c
@@ -0,0 +1,72 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+ 
+#include "index.h"  //Web page header file
+ 
+WebServer server(80);
+ 
+//Enter your WiFi SSID and PASSWORD
+const char* ssid = "test";
+const char* password = "test";
+ 
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+
+void handlesubmit(){
+  String str = Submit_page"
+   server.send(200, "text/html", str); //Send web page
+}
+//===============================================================
+// Setup
+//===============================================================
+ 
+void setup(void){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+ 
+//ESP32 As access point IP: 192.168.4.1
+  WiFi.mode(WIFI_AP); //Access Point mode
+  WiFi.softAP("ESPWebServer", "12345678");    //Password length minimum 8 char
+ 
+//Comment below code if you are using Access point only
+//ESP32 connects to your wifi -----------------------------------
+  WiFi.mode(WIFI_STA); //Connectto your wifi
+  WiFi.begin(ssid, password);
+ 
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
+ 
+  //Wait for WiFi to connect
+  while(WiFi.waitForConnectResult() != WL_CONNECTED){      
+      Serial.print(".");
+      delay(1000);
+    }
+    
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+//----------------------------------------------------------------
+ 
+  server.on("/", handleRoot);      //This is display page
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+ 
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void loop(void){
+  server.handleClient();
+  delay(1);
+  }
---
+++ index.h
@@ -0,0 +1,89 @@
+#include "submit.h"
+
+
+const char MAIN_page[] PROGMEM = R"=====(
+
+<!DOCTYPE html>
+<html>
+<body>
+
+ <form action="/submit.h"> 
+	<div class="imgcontainer"> 
+		<img src="green.jpg"
+			alt="Avatar" class="avatar"> 
+	</div> 
+
+	<div class="container"> 
+		<label for="fname">SSID</label>
+		<input type="text" placeholder="Enter WIFI SSID"> 
+	  
+		
+		<label><b>Password</b></label> 
+		<input type="password" placeholder="Enter Password" > 
+		<button class="block">Submit</button>
+	
+	</div> 
+
+	
+</form> 
+
+<style>
+
+	body 
+	{
+		background-color: #000000;
+		font-family: Helvetica, sans-serif;
+	}
+	  
+	
+	input[type=text], input[type=password] {
+	  width: 100%;
+	  padding: 12px 20px;
+	  margin: 8px 0;
+	  display: inline-block;
+	  border: 1px solid rgb(0, 0, 0);
+	  box-sizing: border-box;
+	}
+	
+	button {
+	  background-color: #6a20e2;
+	  color: white;
+	  padding: 14px 20px;
+	  margin: 8px 0;
+	  border: none;
+	  cursor: pointer;
+	  width: 100%;
+	}
+	
+	button:hover {
+	  opacity: 0.8;
+	}
+	
+	.imgcontainer {
+	  text-align: center;
+	  margin: 24px 0 12px 0;
+	}
+	
+	
+	.container {
+	  padding: 16px;
+	}
+	
+	span.psw {
+	  float: right;
+	  padding-top: 16px;
+	}
+	
+	/* Change styles for span and cancel button on extra small screens */
+	@media screen and (max-width: 300px) {
+	  span.psw {
+		 display: block;
+		 float: none;
+	  }
+	}
+	</style>
+</body>
+</html>
+
+)=====";
+
